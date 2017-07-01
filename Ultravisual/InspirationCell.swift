a44a8175b8e2049de58d5e9d98f634aded660e07
Dang Quoc Huy <huysamdua@yahoo.com>
2017-07-01 15:11:00 +0700

    Featuring Featured Cells

--- Ultravisual/InspirationCell.swift
+++ Ultravisual/InspirationCell.swift
@@ -20,5 +20,18 @@ class InspirationCell: UICollectionViewCell {
       }
     }
   }
+
+  override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+    super.apply(layoutAttributes)
+    
+    let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
+    let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+    
+    let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
+    
+    let minAlpha: CGFloat = 0.3
+    let maxAlpha: CGFloat = 0.75
+    imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+  }
   
 }
